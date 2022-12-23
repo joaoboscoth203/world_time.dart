1a3d0c9255121e9f257a49074f23ce170ad97e0c
João Bosco <joaoboscoth203@yahoo.com.br>
2022-12-23 17:42:22 -0300

    first commit

---
+++ lib/services/world_time.dart
@@ -0,0 +1,49 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:intl/intl.dart';
+
+class WorldTime {
+
+  String location = ''; // location name for the UI
+  String time = ''; // the time in that location
+  String flag = ''; // url to an asset flag icon
+  String url = ''; // location for api endpoint
+  bool isDaytime = false; // true or false if it's daytime or not
+
+  WorldTime({ required this.location, required this.flag, required this.url });
+
+  Future<void> getTime() async {
+
+    try {
+      // make the request
+      var endPoint = Uri.parse('http://worldtimeapi.org/api/timezone/$url');
+      var response = await http.get(endPoint);
+      Map data = jsonDecode(response.body);
+      print(data); // print all data
+
+      // get properties from data
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1,3);
+
+      //print(datetime);
+      print(offset);
+
+
+      //create DateTime object
+      DateTime now = DateTime.parse(datetime);
+
+      //now.subtract(Duration(hours: int.parse(offset)));
+
+      // set the time property
+      isDaytime = now.hour > 6 && now.hour < 18 ? true : false;
+      time = DateFormat.jm().format(now);
+    }
+    catch (e) {
+      print('caught error: $e');
+      time = 'could not get time data';
+
+    }
+
+  }
+
+}
